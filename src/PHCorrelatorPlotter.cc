ccf88e06057b8e0598d56e56da32acdea7279b20
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-11-12 09:45:59 -0500

    Begin creating plotter class

---
+++ src/PHCorrelatorPlotter.cc
@@ -0,0 +1,26 @@
+/// ===========================================================================
+/*! \file   PHCorrelatorPlotter.cc
+ *  \author Derek Anderson
+ *  \date   11.12.2024
+ *
+ *  Class to consolidate plotting routines associated with
+ *  the PHENIX ENC analysis.
+ */
+/// ===========================================================================
+
+#define PHCORRELATORPLOTTER_CC
+
+// class definition
+#include "PHCorrelatorPlotter.h"
+
+
+
+// public methods =============================================================
+
+/* TODO fill in */
+
+// private methods ============================================================
+
+/* TODO fill in */
+
+// end ========================================================================
---
+++ src/PHCorrelatorPlotter.h
@@ -0,0 +1,43 @@
+/// ===========================================================================
+/*! \file   PHCorrelatorPlotter.h
+ *  \author Derek Anderson
+ *  \date   11.12.2024
+ *
+ *  Class to consolidate plotting routines associated with
+ *  the PHENIX ENC analysis.
+ */
+/// ===========================================================================
+
+#ifndef PHCORRELATORPLOTTER_H
+#define PHCORRELATORPLOTTER_H
+
+// c++ utilities
+#include <string>
+
+// forward declarations
+class TCanvas;
+class TFile;
+
+
+
+// ============================================================================
+//! ENC Plotter
+// ============================================================================
+class PHCorrelatorPlotter {
+
+  public:
+
+    PHCorrelatorPlotter()  {};
+    ~PHCorrelatorPlotter() {};
+
+    /* TODO fill in */
+
+  private:
+
+    /* TODO fill in */
+
+};  // end Plotter
+
+#endif
+
+// end ========================================================================
