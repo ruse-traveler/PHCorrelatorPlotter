0892ee2ea1f80e09393a9f8e95f75a2c7c6d1d29
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-12-27 15:41:19 -0500

    Fill in spectrum comparison routine

--- src/PHCorrelatorPlotter.cc
+++ src/PHCorrelatorPlotter.cc
@@ -74,6 +74,14 @@ PHCorrelatorPlotter::PHCorrelatorPlotter(
 // ----------------------------------------------------------------------------
 //! Compare various ENC (or othwerise) spectra
 // ----------------------------------------------------------------------------
+/*! Compares a variety of ENC (or otherwise) spectra from different sources.
+ *
+ *  \param[in]  inputs list of objects to plot and their details
+ *  \param[in]  range  (x, y) ranges to plot
+ *  \param[in]  canvas definition of the canvas to draw on
+ *  \param[out] ofile  file to write to
+ *  \param[in]  header optionally, can provide header for legend
+ */
 void PHCorrelatorPlotter::CompareSpectra(
   const Inputs& inputs,
   const PHEC::Range& range,
@@ -82,7 +90,106 @@ void PHCorrelatorPlotter::CompareSpectra(
   const std::string& header
 ) const {
 
-  /* TODO fill in */
+  // announce start
+  std::cout << "\n -------------------------------- \n"
+            << "  Beginning spectra plotting!\n"
+            << "    Opening inputs:"
+            << std::endl;
+
+  // open inputs
+  std::vector<TFile*> ifiles;
+  std::vector<TH1*>   ihists;
+  for (std::size_t iin = 0; iin < inputs.size(); ++iin) {
+
+    ifiles.push_back(
+      OpenFile(inputs[iin].file, "read")
+    );
+    ihists.push_back(
+      (TH1*) GrabObject( inputs[iin].object, ifiles.back() )
+    );
+    ihists.back() -> SetName( inputs[iin].rename.data() );
+    std::cout << "      File = " << inputs[iin].file << "\n"
+              << "      Hist = " << inputs[iin].object
+              << std::endl;
+
+  }  // end input loop
+
+  // define legend dimensions
+  const std::size_t nlines    = !header.empty() ? ihists.size() + 1 : ihists.size();
+  const float       spacing   = m_baseTextStyle.GetTextStyle().spacing;
+  const float       legheight = PHEC::Tools::GetHeight(nlines, spacing);
+
+  // generate legend vertices
+  PHEC::Type::Vertices vtxleg;
+  vtxleg.push_back(0.3);
+  vtxleg.push_back(0.1);
+  vtxleg.push_back(0.5);
+  vtxleg.push_back((float) 0.1 + legheight);
+
+  // define legend
+  PHEC::Legend legdef;
+  for (std::size_t ihst = 0; ihst < ihists.size(); ++ihst) {
+    legdef.AddEntry( PHEC::Legend::Entry(ihists[ihst], inputs[ihst].legend, "PF") );
+  }
+  legdef.SetVertices( vtxleg );
+  if (!header.empty()) {
+    legdef.SetHeader( header );
+  }
+
+  // create root objects
+  TLegend*   legend = legdef.MakeLegend();
+  TPaveText* text   = m_textBox.MakeTPaveText();
+  std::cout << "    Created legend and text box." << std::endl;
+
+  // set hist styles
+  Styles styles = GenerateStyles( inputs );
+  for (std::size_t ihst = 0; ihst < ihists.size(); ++ihst) {
+    styles[ihst].SetPlotStyle( inputs[ihst].style );
+    styles[ihst].Apply( ihists[ihst] );
+    ihists[ihst] -> GetXaxis() -> SetRangeUser( range.x.first, range.x.second );
+    ihists[ihst] -> GetYaxis() -> SetRangeUser( range.y.first, range.y.second );
+  }
+
+  // set legend/text styles
+  m_baseTextStyle.Apply( legend );
+  m_baseTextStyle.Apply( text );
+  std::cout << "    Set styles." << std::endl;
+
+  // draw plot
+  PHEC::PlotManager manager = PHEC::PlotManager( canvas );
+  manager.MakePlot();
+  manager.Draw();
+  manager.GetTCanvas() -> cd();
+  ihists[0] -> Draw();
+  for (std::size_t ihst = 1; ihst < ihists.size(); ++ihst) {
+    ihists[ihst] -> Draw("same");
+  }
+  legend -> Draw();
+  text   -> Draw();
+  std:: cout << "    Made plot." << std::endl;
+
+  // save output
+  ofile -> cd();
+  for (std::size_t ihst = 0; ihst < ihists.size(); ++ihst) {
+    ihists[ihst] -> Write();
+  }
+  manager.Write();
+  manager.Close();
+  std::cout << "    Saved output." << std::endl;
+
+  // close input files
+  for (std::size_t iin = 0; iin < ifiles.size(); ++iin)  {
+    ifiles[iin] -> cd();
+    ifiles[iin] -> Close();
+  }
+  std::cout << "    Closed input files." << std::endl;
+
+  // announce end
+  std::cout << "  Finished spectra plotting!\n"
+            << " -------------------------------- \n"
+            << endl;
+
+  // exit routine
   return;
 
 }  // end 'CompareSpectra(Inputs&, PHEC::Range&, PHEC::Canvas&, TFile*, std::string&)'
