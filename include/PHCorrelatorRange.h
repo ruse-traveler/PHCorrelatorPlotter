f7706d3311ad43b404895db02a025d6ed274eefb
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-02-05 00:00:33 -0500

    Convert to class, add application method

--- include/PHCorrelatorRange.h
+++ include/PHCorrelatorRange.h
@@ -12,6 +12,8 @@
 
 // c++ utilities
 #include <utility>
+// root libraries
+#include <TAxis.h>
 // plotting utilities
 #include "PHCorrelatorPlotTypes.h"
 
@@ -25,57 +27,109 @@ namespace PHEnergyCorrelator {
   /*! This struct groups together the start/stop
    *  points for all 3 axes for convenience.
    */
-  struct Range {
+  class Range {
 
-    // members
-    Type::Interval x;
-    Type::Interval y;
-    Type::Interval z;
+    public:
 
-    // ------------------------------------------------------------------------
-    //! default ctor
-    // ------------------------------------------------------------------------
-    Range() {
-      x = std::make_pair(0.0, 1.0);
-      y = std::make_pair(0.0, 1.0);
-      z = std::make_pair(0.0, 1.0);
-    };
+      // accessor
+      //   - FIXME should be in Constants
+      enum Axis {X, Y, Z};
 
-    // ------------------------------------------------------------------------
-    //! default dtor
-    // ------------------------------------------------------------------------
-    ~Range() {};
+    private:
 
-    // ------------------------------------------------------------------------
-    //! ctor accepting only x range
-    // ------------------------------------------------------------------------
-    Range(const Type::Interval& xrange) {
-      x = xrange;
-    }
+      // members
+      Type::Interval x;
+      Type::Interval y;
+      Type::Interval z;
 
-    // ------------------------------------------------------------------------
-    //! ctor accepting x and y range
-    // ------------------------------------------------------------------------
-    Range(
-      const Type::Interval& xrange,
-      const Type::Interval& yrange
-    ) {
-      x = xrange;
-      y = yrange;
-    }
+    public:
 
-    // ------------------------------------------------------------------------
-    //! ctor accepting x, y, and z range
-    // ------------------------------------------------------------------------
-    Range(
-      const Type::Interval& xrange,
-      const Type::Interval& yrange,
-      const Type::Interval& zrange
-    ) {
-      x = xrange;
-      y = yrange;
-      z = zrange;
-    }
+      // ----------------------------------------------------------------------
+      //! Getters
+      // ----------------------------------------------------------------------
+      Type::Interval GetX() const {return x;}
+      Type::Interval GetY() const {return y;}
+      Type::Interval GetZ() const {return z;}
+
+      // ----------------------------------------------------------------------
+      //! Setters
+      // ----------------------------------------------------------------------
+      void SetX(const Type::Interval& range) {x = range;}
+      void SetY(const Type::Interval& range) {y = range;}
+      void SetZ(const Type::Interval& range) {z = range;}
+
+      // ----------------------------------------------------------------------
+      //! Apply a range to a TAxis
+      // ----------------------------------------------------------------------
+      void Apply(const Axis axis, TAxis* to_range) const {
+
+        switch (axis) {
+
+          case X:
+            to_range -> SetRangeUser(x.first, x.second);
+            break;
+
+          case Y:
+            to_range -> SetRangeUser(y.first, y.second);
+            break;
+
+          case Z:
+            to_range -> SetRangeUser(z.first, z.second);
+            break;
+
+          default:
+            /* do nothing */
+            break;
+
+        }
+        return;
+
+      }  // end 'Apply(Axis, TAxis*)'
+
+      // ----------------------------------------------------------------------
+      //! default ctor
+      // ----------------------------------------------------------------------
+      Range() {
+        x = std::make_pair(0.0, 1.0);
+        y = std::make_pair(0.0, 1.0);
+        z = std::make_pair(0.0, 1.0);
+      };
+
+      // ----------------------------------------------------------------------
+      //! default dtor
+      // ----------------------------------------------------------------------
+      ~Range() {};
+
+      // ----------------------------------------------------------------------
+      //! ctor accepting only x range
+      // ----------------------------------------------------------------------
+      Range(const Type::Interval& xrange) {
+        x = xrange;
+      }
+
+      // ----------------------------------------------------------------------
+      //! ctor accepting x and y range
+      // ----------------------------------------------------------------------
+      Range(
+        const Type::Interval& xrange,
+        const Type::Interval& yrange
+      ) {
+        x = xrange;
+        y = yrange;
+      }
+
+      // ----------------------------------------------------------------------
+      //! ctor accepting x, y, and z range
+      // ----------------------------------------------------------------------
+      Range(
+        const Type::Interval& xrange,
+        const Type::Interval& yrange,
+        const Type::Interval& zrange
+      ) {
+        x = xrange;
+        y = yrange;
+        z = zrange;
+      }
 
   };  // end Range
 
