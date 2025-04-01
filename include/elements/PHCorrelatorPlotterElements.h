b8566d482a7777fb9e9b36949d43412aa0c243f2
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-04-01 15:00:01 -0400

    Rename directory and add all-in-one header for category

---
+++ include/elements/PHCorrelatorPlotterElements.h
@@ -0,0 +1,32 @@
+/// ============================================================================
+/*! \file    PHCorrelatorPlotterElements.h
+ *  \authors Derek Anderson
+ *  \date    04.01.2025
+ *
+ *  All-in-one header for PHCorrelatorPlotter
+ *  elements.
+ */
+/// ============================================================================
+
+#ifndef PHCORRELATORPLOTTERELEMENTS_H
+#define PHCORRELATORPLOTTERELEMENTS_H
+
+#include "PHCorrelatorCanvas.h"
+#include "PHCorrelatorCanvasManager.h"
+#include "PHCorrelatorLegend.h"
+#include "PHCorrelatorPad.h"
+#include "PHCorrelatorPadOpts.h"
+#include "PHCorrelatorPlotInput.h"
+#include "PHCorrelatorPlotOpts.h"
+#include "PHCorrelatorPlotShape.h"
+#include "PHCorrelatorPlotTools.h"
+#include "PHCorrelatorPlotTypes.h"
+#include "PHCorrelatorProjection.h"
+#include "PHCorrelatorRange.h"
+#include "PHCorrelatorShape.h"
+#include "PHCorrelatorStyle.h"
+#include "PHCorrelatorTextBox.h"
+
+#endif
+
+/// end ========================================================================
